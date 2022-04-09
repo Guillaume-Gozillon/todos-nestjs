28519c127f07e4b4e2a2dcde41916b46adc3c04b
guillaumegozillon <guillaumegozillon@gmail.com>
2022-04-09 21:06:59 +0200

    Add: controllers

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
+import { TodosService } from "./todos.service";
 
-@Controller('todos')
-export class TodosController {}
+@Controller("todos")
+export class TodosController {
+  constructor(private readonly TodosService: TodosService) {
+  }
+
+  @Get()
+  findAll(): Promise<any[]> {
+    // @ts-ignore
+    return this.TodosService.findAll();
+  }
+}
